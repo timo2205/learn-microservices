99c5d4954f9d473350dc44aef21f94062c541f0b
timo2205 <timothy_hrj@yahoo.com>
2025-02-24 21:52:14 +0700

    add restaurant-service

---
+++ restaurant-service/src/main/java/com/timothy/restaurant/RestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.timothy.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantServiceApplication.class, args);
+	}
+
+}
--- user-service/src/main/java/com/timothy/user_service/service/UserService.java
+++ user-service/src/main/java/com/timothy/user_service/service/UserService.java
@@ -1,5 +1,7 @@
 package com.timothy.user_service.service;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public class UserService {
 }
